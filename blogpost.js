54f827ed136de2b99a0c74bd0deaa32663470064
Mir_Rom <rborisovic74@gmail.com>
2024-04-22 21:12:06 +0300

    style posts and post by id, implement error handling

--- blogpost.js
+++ blogpost.js
@@ -1,18 +1,21 @@
 import express from 'express'
 import fs from 'fs/promises'
 const app = express()
+app.use('/images', express.static('images'))
 
 app.get('/posts/', async (req, res) => {
-	const posts = JSON.parse(await fs.readFile('posts.json', 'utf8'))
-	res.json({ code: 0, posts })
+	const data = JSON.parse(await fs.readFile('posts.json', 'utf8'))
+	res.json({ code: 0, data })
 })
 app.get('/posts/:id', async (req, res) => {
 	const posts = JSON.parse(await fs.readFile('posts.json', 'utf-8'))
-	for (const post of posts) {
-		if (post.id == req.params.id) {
-			res.json({ code: 0, post })
-		}
+	const data = posts.find((post) => post.id === req.params.id)
+
+	if (!data) {
+		res.json({ code: 1, error: 'not found' })
+		return
 	}
+	res.json({ code: 0, data })
 })
 
 app.listen(3000)
